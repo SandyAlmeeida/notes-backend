00d942b0926343621d20b8e520314617b9ba613d
Sandy Almeida <contato.sandyalmeida@gmail.com>
2024-03-22 00:37:10 -0300

    Creating the return of tags within the note and notes within the tag.

--- routes/notes.js
+++ routes/notes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Note = require('../models/note');
 const NoteTag = require('../models/notetag');
+const Tag = require('../models/tag')
 
 router.get('/', async (req, res) => {
   try {
@@ -10,6 +11,22 @@ router.get('/', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Error on search notes.' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const note = await Note.findByPk(id);
+    const noteTags = await NoteTag.findAll( {where: { noteId: id } });
+    const tagsIds = noteTags.map((tag) => tag.tagId)
+    const tags = await Tag.findAll( {where: { id: tagsIds } });
+    note.dataValues.tags = tags;
+    console.log(note);
+    res.json(note);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error on find note.' });
   }
 });
 
@@ -36,11 +53,17 @@ router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const { title, description, tags } = req.body;
   try {
-    const newNote = await Note.update({ title, description }, { where: {id} });
+    const editNote = await Note.update({ title, description }, { where: {id} });
     if (tags && tags.length > 0) {
-      await newNote.setTags(tags);
+      const createNoteTag = [];
+      tags.forEach(tagId => {
+        const response = { noteId: editNote.id, tagId };
+        createNoteTag.push(response);
+      });
+      NoteTag.destroy({ where: { nodeId: id }})
+      await NoteTag.bulkCreate(createNoteTag);
     }
-    res.status(201).json(newNote);
+    res.status(201).json(editNote);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Error on create note.' });
--- routes/tags.js
+++ routes/tags.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Tag = require('../models/tag');
+const NoteTag = require('../models/notetag');
+const Note = require('../models/note');
 
 router.get('/', async (req, res) => {
   try {
@@ -9,6 +11,21 @@ router.get('/', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Error on search tags.' });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const tag = await Tag.findByPk(id);
+    const noteTags = await NoteTag.findAll( {where: { tagId: id } });
+    const notesIds = noteTags.map((note) => note.noteId)
+    const notes = await Note.findAll( {where: { id: notesIds } });
+    tag.dataValues.notes = notes;
+    res.json(tag);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error on find tag.' });
   }
 });
 
